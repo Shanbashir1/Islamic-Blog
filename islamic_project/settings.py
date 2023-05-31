5829c8495900d2c10c4d10a29a18fc5daf1e2394
Shanbashir1 <>
2023-05-31 20:36:31 +0000

    Added images to background and created navbar and index page

--- islamic_project/settings.py
+++ islamic_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-h9sr0wirgk!_xkleve0ym^5piuva-20f#!tzl!ii6^s*y*e*d7
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '8000-shanbashir1-islamic-blog-700r9ifood.us2.codeanyapp.com']
+    '8000-shanbashir1-islamic-blog-bb63548vf6.us2.codeanyapp.com']
 
 
 # Application definition
@@ -148,6 +148,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
